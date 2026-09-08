5cda24256c5324efeafb8aff7493c8aed1f559e4
seva0174 <vsevolodcinque@gmail.com>
2026-09-08 13:11:01 +0200

    config d Axios et React router

---
+++ fast_food_frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,19 @@
+import { useContext } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
+
+export const ProtectedRoute = ({ allowedRoles }) => {
+  const { user, loading } = useContext(AuthContext);
+
+  if (loading) return <div>Chargement...</div>;
+
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+
+  if (allowedRoles && !allowedRoles.includes(user.role)) {
+    return <Navigate to="/" replace />; // Redirige si le rôle n'a pas la permission
+  }
+
+  return <Outlet />;
+};
---
+++ fast_food_frontend/src/context/AuthProvider.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import { AuthContext } from './AuthContext';
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(() => {
+    const storedUser = localStorage.getItem('user');
+    return storedUser ? JSON.parse(storedUser) : null;
+  });
+
+  const [loading] = useState(false);
+
+  const login = (token, userData) => {
+    localStorage.setItem('token', token);
+    localStorage.setItem('user', JSON.stringify(userData));
+    setUser(userData);
+  };
+
+  const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
